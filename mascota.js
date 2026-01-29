7abdbce3bb79e3c14eee6907941c6fb87ba72e5f
sebastian mory ricse <20223774@aloe.ulima.edu.pe>
2026-01-29 17:55:23 -0500

    Add horizontal movement animation for mascota
    Implement animation for mascota element moving horizontally.

--- mascota.js
+++ mascota.js
@@ -1,1 +1,19 @@
+let mascota = document.getElementById("mascota");
+let x = 0;
+let direccion = 1;
+
+setInterval(() => {
+  x += 2 * direccion;
+  mascota.style.left = x + "px";
+
+  if (x > window.innerWidth - 50) {
+    direccion = -1;
+    mascota.style.transform = "scaleX(-1)";
+  }
+
+  if (x < 0) {
+    direccion = 1;
+    mascota.style.transform = "scaleX(1)";
+  }
+}, 30);
 
